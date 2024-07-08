0f710b4a63128eb290455891c4b2a1eb49f09aef
Jhonatan Alves <jhonatan4alves@gmail.com>
2024-07-08 15:02:17 -0300

    fix: logo icon size

--- src/app/components/Navbar/Desktop.js
+++ src/app/components/Navbar/Desktop.js
@@ -1,33 +1,33 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Desktop() {
   const router = useRouter();
-  const [activeSection, setActiveSection] = useState('');
+  const [activeSection, setActiveSection] = useState("");
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = document.querySelectorAll('section');
-      let currentSection = 'Hero';
+      const sections = document.querySelectorAll("section");
+      let currentSection = "Hero";
 
       sections.forEach((section) => {
         const sectionTop = section.offsetTop;
         if (window.scrollY >= sectionTop - 100) {
-          currentSection = section.getAttribute('id');
+          currentSection = section.getAttribute("id");
         }
       });
 
       setActiveSection(currentSection);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -36,50 +36,72 @@ export default function Desktop() {
     if (element) {
       window.scrollTo({
         top: element.offsetTop - 50,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     }
   };
 
   return (
     <nav className="hidden lg:block w-[100vw]">
-      <div className='fixed w-full top-0 z-50 h-[8px] bg-customPurple100'></div>
-      <div className='fixed w-[96%] ml-10 top-2 z-50 h-[79px] bg-customBlue100 shadow-lg flex items-center justify-between px-20 rounded-full'>
+      <div className="fixed w-full top-0 z-50 h-[8px] bg-customPurple100"></div>
+      <div className="fixed w-[96%] ml-10 top-2 z-50 h-[79px] bg-customBlue100 shadow-lg flex items-center justify-between px-20 rounded-full">
         <div className="flex items-center justify-between w-full px-10 text-xl">
           <button
-            className={`nav-link ${activeSection === 'Hero' ? 'active' : ''} mx-2`}
-            onClick={() => smoothScrollToSection('Hero')}
+            className={`nav-link ${
+              activeSection === "Hero" ? "active" : ""
+            } mx-2`}
+            onClick={() => smoothScrollToSection("Hero")}
           >
             Início
           </button>
           <button
-            className={`nav-link ${activeSection === 'About' ? 'active' : ''} mx-2`}
-            onClick={() => smoothScrollToSection('About')}
+            className={`nav-link ${
+              activeSection === "About" ? "active" : ""
+            } mx-2`}
+            onClick={() => smoothScrollToSection("About")}
           >
             Sobre
           </button>
           <button
-            className={`nav-link ${activeSection === 'Services' ? 'active' : ''} mx-2`}
-            onClick={() => smoothScrollToSection('Services')}
+            className={`nav-link ${
+              activeSection === "Services" ? "active" : ""
+            } mx-2`}
+            onClick={() => smoothScrollToSection("Services")}
           >
             Serviços
           </button>
-          <div className="mx-2"><Image src="/Logoi9.png" alt='logo' width={64} height={64} className='' /></div>
+
+          <div className="mx-2">
+            <Image
+              src="/Logoi9.png"
+              alt="logo"
+              width={110}
+              height={110}
+              className=""
+            />
+          </div>
+
           <button
-            className={`nav-link ${activeSection === 'Expirience' ? 'active' : ''} mx-2`}
-            onClick={() => smoothScrollToSection('Expirience')}
+            className={`nav-link ${
+              activeSection === "Expirience" ? "active" : ""
+            } mx-2`}
+            onClick={() => smoothScrollToSection("Expirience")}
           >
             Experiência
           </button>
           <button
-            className={`nav-link ${activeSection === 'Testimonials' ? 'active' : ''} mx-2`}
-            onClick={() => smoothScrollToSection('Testimonials')}
+            className={`nav-link ${
+              activeSection === "Testimonials" ? "active" : ""
+            } mx-2`}
+            onClick={() => smoothScrollToSection("Testimonials")}
           >
             Testemunhos
           </button>
           <button
-            className={`nav-link ${activeSection === 'Contact' ? 'active' : ''} mx-2`}
-            onClick={() => smoothScrollToSection('Contact')}
+            className={`nav-link ${
+              activeSection === "Contact" ? "active" : ""
+            } mx-2`}
+            onClick={() => smoothScrollToSection("Contact")}
           >
             Contato
           </button>
