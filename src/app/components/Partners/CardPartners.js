6c00d56fc39adbf268fe55d7c6cc28a78b426a2b
Jhonatan Alves <jhonatan4alves@gmail.com>
2024-08-27 09:59:09 -0300

    ajustando responsividade caroussel de empresas clientes

--- src/app/components/Partners/CardPartners.js
+++ src/app/components/Partners/CardPartners.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Card = ({imageUrl }) => {
     return (
         <div className="partners-div">
-            <Image src={imageUrl} alt='company' width={98} height={98} className="card-size" />
+            <Image src={imageUrl} alt='company' width={68} height={68} className="card-size" />
         </div>
     );
 };
--- src/app/components/Partners/HomePartners.js
+++ src/app/components/Partners/HomePartners.js
@@ -5,9 +5,9 @@ import PartnersCarousel from "./PartnersCarousel"
 export default function HomePartners(){
     return(
         <section>
-            <div className="w-[100vw] px-10 mt-20">
+            <div className="w-full px-3 md:px-10 lg:px-10 mt-20">
                 <div className="flex align-center justify-center">
-                    <h1 className="font-alfa text-5xl font-semibold mb-14 text-white ">Empresas que confiam na I9</h1>
+                    <h1 className="font-alfa px-5 text-center text-[30px] md:text-[35px] lg:text-[35px] font-normal mb-14 text-white ">Empresas que confiam na I9</h1>
                 </div>
                 <PartnersCarousel />
             </div>
